9fe3aa8854c2fcc2b779b38b6de4eecc0c62d128
Jacob Payne <jacobapayne@gmail.com>
2017-06-02 20:09:14 -0700

    Start Tests 1/4

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var SimpleAccount = artifacts.require("./SimpleAccount.sol");
+var Payroll = artifacts.require("./Payroll.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleAccount);
+  deployer.deploy(Payroll);
 };
--- test/payroll.js
+++ test/payroll.js
@@ -1,73 +1,71 @@
-var Payroll = artifacts.require("./Payroll.sol");
+
+const Payroll = artifacts.require('./Payroll.sol')
 
 contract('Payroll', function (accounts) {
 
-    describe('constructor', function () {
-        return Payroll.deployed()
-            .then(function (payroll) {
-                return payroll.owner.call(accounts[0]);
-            }).then(function (owner) {
-                assert.equal(owner, accounts[0]);
-            });
+    describe('constructor', async function () {
+        let payroll = await Payroll.deployed();  
+        let owner = await payroll.owner.call(accounts[0])      
+        assert.equal(owner, accounts[0]);
     })
 
     describe('Payable Calls', function () {
         describe('Default', function () {
             it('should receive tokens', async function () {
                 let payroll = await Payroll.deployed();
-                await payroll.send(100);
-                
-                let balance = web3.eth.getBalance(payroll.address).toNumber();
-                assert.equal(balance, 100);
+                return payroll.send(100).then(() => {
+                    let balance = web3.eth.getBalance(payroll.address).toNumber();
+                    assert.equal(balance, 100);
+                })
             })           
         })
         describe('.addFunds() payable', function () {
-            throw new Error('Unimplemented')
+            it('should pass')
         });
     })
     describe('Public Calls', function () {
         describe('.getEmployeeCount() constant returns (uint256)', function () {
-            throw new Error('Unimplemented')
+            it('should pass')
         });
         describe('.getEmployee(uint256 employeeId) constant returns (address employee)', function () {
-            throw new Error('Unimplemented')
+            it('should pass')
         });
         describe('.calculatePayrollBurnrate() constant returns (uint256)', function () {
-            throw new Error('Unimplemented')
+            it('should pass')
         });
         describe('.calculatePayrollRunway() constant returns (uint256)', function () {
-            throw new Error('Unimplemented')
+            it('should pass')
         }); 
 
     })
 
     describe('Employer Calls', function () {
         describe('addEmployee(address accountAddress, address[] allowedTokens, uint256 initialYearlyUSDSalary)', function () {
-            throw new Error('Unimplemented')
+            it('should pass')
         });
         describe('setEmployeeSalary(uint256 employeeId, uint256 yearlyUSDSalary)', function () {
-            throw new Error('Unimplemented')
+            it('should pass')
         });
         describe('removeEmployee(uint256 employeeId)', function () {
-            throw new Error('Unimplemented')
+            it('should pass')
         });
         describe('setOracle(address oracle)', function () {
-            throw new Error('Unimplemented')
+            it('should pass')
         });
     })
 
     describe('Employee Calls', function () {
         describe('determineAllocation(address[] tokens, uint256[] distribution)', function () {
-            throw new Error('Unimplemented')
+            it('should pass')
         });
         describe('payday(); // only callable once a month', function () {
-            throw new Error('Unimplemented')
+            it('should pass')
         });
     })
 
     describe('Oracle Calls', function () {
         describe('setExchangeRate(address token, uint256 usdExchangeRate)', function () {
-            throw new Error('Unimplemented')
+            it('should pass')
         })
     })
 
