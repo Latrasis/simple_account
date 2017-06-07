309cfa7ab4f1ae0ad3a19cae361171e54c7572ae
Jacob Payne <jacobapayne@gmail.com>
2017-06-06 23:32:37 -0700

    Setup EmployeeType + Tweak SalaryType

--- contracts/EmployeeType.sol
+++ contracts/EmployeeType.sol
@@ -2,16 +2,79 @@ pragma solidity ^0.4.8;
 
 import "./SalaryType.sol";
 
+
 /// An Employee
 library EmployeeType {
   using SalaryType for SalaryType.Self;
+  using EmployeeType for EmployeeType.Self;
 
   struct Self {
-    /// Array of Token Salaries
-    SalaryType.Self[] allowedTokens;
+    // Employee Address
+    address employee;
+    // Total salary per period
+    uint256 totalSalary;
     // Defined in USD
-    uint256 totalDailySalary;
+    uint256 paymentPeriod;
+    // Employment Date
+    uint256 startTime;
+    // Array of Token Salaries
+    SalaryType.Self[] tokenSalaries;
   }
+
+  /// Reset Self
+  function reset(Self storage self) internal {
+      self.tokenSalaries.length = 0;
+      self.totalSalary = 0;
+      self.paymentPeriod = 0;
+      self.startTime = today();
+  }
+  
+  function owed(Self storage self) internal constant returns (uint owedPayments) {
+      for (uint i = 0; i < self.tokenSalaries.length; i++) {
+          owedPayments += self.tokenSalaries[i].allocationOwed(self.paymentPeriod);
+      }
+  }
+
+  /// Update Salary
+  /// tokens: List of ERC20 token addresses, must correlate with tokenSalary index
+  /// allocations: Correlated by index, amount in base token, where sum of all allocations = totalSalary
+  /// totalSalary: Total salary per period
+  /// paymentPeriod: Period of days per salary
+  function updateSalary(Self storage self, address[] tokens, uint256[] allocations, uint256 totalSalary, uint256 paymentPeriod) internal returns (bool error) {
+    // Check params
+    if (tokens.length != allocations.length || totalSalary == 0 || paymentPeriod == 0) return true;
+
+    // Check if any salary is still owed
+    if (self.owed() > 0) return true;
+    // Clear Data
+    self.reset();
+
+    // Prepare Array
+    self.tokenSalaries.length = tokens.length;
+    // Prepare Sum Check
+    uint256 sumCheck = 0;
+    for (uint256 i = 0; i < tokens.length; i++) {
+        // Check for overflows, but return error
+        var nextSum = sumCheck + allocations[i];
+        if (nextSum < sumCheck) return true;
+        sumCheck = nextSum;
+
+        self.tokenSalaries[i] = SalaryType.Self(tokens[i], 0, allocations[i], today());
+    }
+
+    // If SumCheck does not equal totalSalary, this is an invalid Salary
+    if (sumCheck != totalSalary) return true;
+
+    // At this point we can update the rest
+    self.totalSalary = totalSalary;
+    self.paymentPeriod = paymentPeriod;
+  }
+
+  // determines today's index.
+  function today() private constant returns (uint) {
+    return now / 1 days;
+  }
+
 
 
 }
--- contracts/SalaryType.sol
+++ contracts/SalaryType.sol
@@ -4,12 +4,14 @@ import "zeppelin/token/ERC20Basic.sol";
 
 /// A Token Salary
 library SalaryType {
-
+  using SalaryType for SalaryType.Self;
   struct Self {
     // An ERC20 basic token
     address token;
     // Token Salary
     uint amount;
+    // Allocation in Base Token
+    uint allocation;
     // Time since last paycheck
     uint lastPaycheckDay;
   }
@@ -17,38 +19,55 @@ library SalaryType {
   /// Reset Self
   function reset(Self storage self) internal {
       self.amount = 0;
+      self.allocation = 0;
       self.lastPaycheckDay = today();
   } 
 
   /// Update Token Salary given:
-  /// allocation: value in baseToken
   /// exchangeValue: where exchangeValue * allocation = Self.amount
-  function allocateBy(Self storage self, uint256 allocation, uint256 exchangeValue) internal returns (bool error) {
+  function updateValue(Self storage self, uint256 exchangeValue) internal returns (bool error) {
     // Checks for Token address and valid params
     if (self.token == 0) return true;
     // If allocation or exchange value equal zero, assume zero amount
-    if (allocation == 0 || exchangeValue == 0) {
+    if (self.allocation == 0 || exchangeValue == 0) {
         self.amount = 0;
     }
 
     // TODO: Need to handle conversion checks for accuracy
-    self.amount = exchangeValue * allocation;
+    self.amount = exchangeValue * self.allocation;
     return false;
   }
 
-  /// Issue paycheck based on days passed
-  /// employee: address to send paycheck to
-  /// paymentPeriod: minimum period of days between paychecks
-  function issuePaycheck(Self storage self, address employee, uint paymentPeriod) internal returns (bool error) {
-
+  /// Check how many paychecks owed
+  function paychecksOwed(Self storage self, uint paymentPeriod) internal constant returns (uint) {
     // Get Time Passed, throw if minimum paymentPeriod hasn't passed
     var timePassed = today() - self.lastPaycheckDay;
 
     // TODO, manage possible overflow
     // timePassed = self.lastPaycheckDay > today() ? (0-1) - timePassed : timePassed;
-    if (timePassed < paymentPeriod) return true;
-    // Handle missed paychecks
-    var paymentsOwed = timePassed / paymentPeriod;
+    if (timePassed < paymentPeriod) return 0;
+
+    // Amount of owed paychecks
+    return timePassed / paymentPeriod;
+  }
+
+  // Check how much in base salary (USD) is owed
+  function allocationOwed(Self storage self, uint paymentPeriod) internal constant returns (uint) {
+    return self.paychecksOwed(paymentPeriod) * self.allocation;
+  }
+
+  /// Issue paycheck based on days passed
+  /// employee: address to send paycheck to
+  /// paymentPeriod: minimum period of days between paychecks
+  function issuePaycheck(Self storage self, address employee, uint paymentPeriod, uint exchangeValue) internal returns (bool error) {
+
+    // Update Value
+    self.updateValue(exchangeValue);
+
+    // Check paychecks owed
+    var paymentsOwed = self.paychecksOwed(paymentPeriod);
+    if (paymentsOwed == 0) return true;
+
     var sumOwed = self.amount * paymentsOwed;
 
     var token = ERC20Basic(self.token);
