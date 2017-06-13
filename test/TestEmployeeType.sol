a23f6fea6b0d8b0ed5d89be67203b0d1f569a880
Jacob Payne <jacobapayne@gmail.com>
2017-06-12 22:17:11 -0700

    Finish Test Types Setup

--- test/TestEmployeeType.sol
+++ test/TestEmployeeType.sol
@@ -3,9 +3,29 @@ pragma solidity ^0.4.2;
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/lib/EmployeeType.sol";
+import "../contracts/lib/SalaryType.sol";
 import "zeppelin/token/SimpleToken.sol";
 
 contract TestEmployeeType {
+    using SalaryType for SalaryType.Self;
+    using EmployeeType for EmployeeType.Self;
+    
+    // Self::reset()
+    function testShouldReset() {
+        // Unimplemented
+        throw;
+    }
 
+    // Self::owed()
+    function testShouldGetOwed() {
+        // Unimplemented
+        throw;
+    }
+
+    // self.updateSalary()
+    function testShouldUpdateSalary() {
+        // Unimplemented
+        throw;
+    }
 
 }
--- test/TestSalaryType.sol
+++ test/TestSalaryType.sol
@@ -7,31 +7,36 @@ import "zeppelin/token/SimpleToken.sol";
 
 contract TestSalaryType {
     using SalaryType for SalaryType.Self;
-    
+
     // Self::reset()
     function testShouldReset() {
         // Unimplemented
+        throw;
     }
 
     // Self::paychecksOwed()
     function testShouldGetPaychecksOwed() {
         // Unimplemented
+        throw;
     }
 
     // Self::allocationOwed()
     function testShouldGetAllocationOwed() {
         // Unimplemented
+        throw;
     }
     
 
     // self.updateValue()
     function testShouldUpdateValue() {
         // Unimplemented
+        throw;
     }
 
     // self.issuePaycheck()
     function shouldIssuePaycheck() {
         // Unimplemented
+        throw;
     }
 
 }
